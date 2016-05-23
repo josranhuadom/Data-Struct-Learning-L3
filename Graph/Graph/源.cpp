bd528170ca65980284815d4bed0f4233431eb98e
josranhuadom <Jkjoshuaking@hotmail.com>
2016-05-23 23:52:58 +0800

    12 changes and what the hell!

--- Graph/Graph/源.cpp
+++ Graph/Graph/源.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define MVNum 100
 #define MaxInt 0
+#define Int 1
 
 /*int VerTexType;
 int ArcType;*/
@@ -15,7 +16,7 @@ struct GraphAM
 
 void CreateUDN(GraphAM &G)
 {
-	int i, j, k, v1, v2, w; //i,j,k用于计数，v1,v2用于储存输入一条边依附的顶点，w用于储存输入的全职
+	int i, j, k, v1, v2;     //i,j,k用于计数，v1,v2用于储存输入一条边依附的顶点
 	cout << "请输入……" << endl;
 	cin >> G.vexnum >> G.arcnum;
 	for (i = 0; i < G.vexnum; ++i)
@@ -25,8 +26,10 @@ void CreateUDN(GraphAM &G)
 			G.arcs[i][j] = MaxInt;
 	for (k = 0; k < G.arcnum; ++k)
 	{
-		cin >> v1 >> v2 >> w;
-
+		cin >> v1 >> v2;
+		i = v1; j = v2;
+		G.arcs[i][j] = Int;
+		G.arcs[j][i] = G.arcs[i][j];
 	}
 }
 
